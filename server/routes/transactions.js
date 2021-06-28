c60bc1ab555d8069088d53663b95544a06adfa0c
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-28 01:23:51 -0500

    added edit transaction route

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -38,6 +38,37 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+// @PUT - /api/v1/transactions/:id - Edit transaction - Public
+router.put('/:id', async(req, res)=>{
+    const {text, amount} = req.body;
+
+    const transactionFields = {};
+    if(text) transactionFields.text = text;
+    if(amount) transactionFields.amount = amount;
+
+    try{
+        let transaction = await Transaction.findById(req.params.id);
+
+        if(!transaction) return res.status(404).json({msg:"transaction not found"})
+
+        transaction = await Transaction.findByIdAndUpdate(
+            req.params.id,
+            {$set: transactionFields},
+            {new: true},
+        );
+
+        return res.status(201).json({
+          success: true,
+          data: transaction
+        });
+      }catch (error) {
+        return res.status(500).json({
+        success: false,
+        error: "Server Error"
+      });
+    }
+})
+
 // @DELETE - /api/v1/transactions/:id - Delete transaction - Public
 router.delete("/:id", async (req, res, next) => {
   try {
